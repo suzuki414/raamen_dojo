010b2dd2937fb2b4e4a494505190bd9f373aff22
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-02-06 07:59:21 +0000

    複数権限でのdevise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :members,skip: [:passwords], controllers: {
+    registrations: "member/registrations",
+    sessions: 'member/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
