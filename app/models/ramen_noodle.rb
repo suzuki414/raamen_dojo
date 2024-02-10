cd1fe1505f7b6135a55a81d02df69d9984f06950
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-02-10 05:14:17 +0000

    5段階評価　作成

--- app/models/ramen_noodle.rb
+++ app/models/ramen_noodle.rb
@@ -5,4 +5,9 @@ class RamenNoodle < ApplicationRecord
   has_many :ramen_noodle_tags, dependent: :destroy
 
   has_one_attached :post_image
+
+  validates :average_rating, numericality: {
+  less_than_or_equal_to: 5,
+  greater_than_or_equal_to: 1}, presence: true
+
 end
