3b7588b1b1c86fb71ed72df5a74761bbbbccaded
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-02-21 01:45:55 +0000

    いいね機能　非同期通信化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -6,15 +6,18 @@ module Public
       ramen_noodle = RamenNoodle.find(params[:ramen_noodle_id])
       favorite = current_member.favorites.new(ramen_noodle_id: ramen_noodle.id)
       favorite.save
-      redirect_to ramen_noodle_path(ramen_noodle)
+      respond_to do |format|
+        format.js { @ramen_noodle = ramen_noodle }
+      end
     end
 
     def destroy
       ramen_noodle = RamenNoodle.find(params[:ramen_noodle_id])
       favorite = current_member.favorites.find_by(ramen_noodle_id: ramen_noodle.id)
       favorite.destroy
-      redirect_to ramen_noodle_path(ramen_noodle)
+      respond_to do |format|
+        format.js { @ramen_noodle = ramen_noodle }
+      end
     end
-
   end
 end
