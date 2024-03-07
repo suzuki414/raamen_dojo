66b6743db6ea3666af2094977b12e7c252d285ed
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-03-07 01:44:01 +0000

    ゲスト会員機能　修正

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,7 +1,7 @@
 module Public
   class MembersController < ApplicationController
     before_action :authenticate_member!, only: [:my_page, :edit, :withdraw, :complete, :unsubscribe]
-    before_action :ensure_guest_member, only: [:edit, :withdraw, :complete, :unsubscribe]
+    before_action :ensure_guest_member, only: [:withdraw]
 
     def index
       if params[:old]
@@ -26,6 +26,10 @@ module Public
     def edit
       @member = current_member
     end
+    
+    def unsubscribe
+      @member = current_member
+    end  
 
     def withdraw
       @member = Member.find(current_member.id)
@@ -40,7 +44,7 @@ module Public
     def ensure_guest_member
       @member = Member.find(current_member.id)
       if @member.guest_member?
-        redirect_to request.referer, notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+        redirect_to request.referer, notice: "ゲスト会員は、会員情報の編集内容保存および退会処理ができません。"
       end
     end
 
